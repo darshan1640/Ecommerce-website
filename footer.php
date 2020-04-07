fca7c183aa99990b4dc4303eb90673227b305742
darshan1640 <47364927+darshan1640@users.noreply.github.com>
2020-04-08 00:20:53 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,40 @@
+<footer class="main-footer" style="background-color:#051b31;color:white;">
+    
+    <div class="container">
+				<table  cellspacing="100">
+          <tr>
+            <th style="padding:0px 300px 0px 0px;"><h3><u>DressedUp</h3></th>
+            <th style="padding:0px 300px 0px 0px;"><h3><u>Product</h3></th>
+            <th style="padding:0px 300px 0px 0px;"><h3><u>Company</h3></th>
+        </tr>
+        <tr>
+          <td>A-38,Saif Soc. near JB Diamond Circle LH Road Varachha, Surat</td>
+          <td><a href="category.php?category=lehenga"> Category </td>
+          <td><a href="#">ABOUT US</a></td>
+        </tr>
+        <tr>
+          <td><a href="#">9825190839 , 8905036936</a></td>
+          <td></td>
+          <td><a href="login.php">SignIn</a></td>
+        </tr>
+        <tr>
+          <td><a href="#">Zalavadiyadarshan@gmail.com</a></td>
+          <td></td>
+          <td><a href="#">Contact Us<br> </a></td>
+        </tr>
+        <tr>
+          <td style="padding:0px 300px 15px 0px;"><a href="#">Priteshsaliya001@gmail.com</a></td>
+          <td></td>
+          <td></td>
+        </tr>
+
+</table>
+    </div>
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2020 Brought to You By Dressed Up</strong>
+    </div>
+</footer>
+
